e92f5d1752b847b8f472536dca0ec00805b733db
4anth0ny1 <acatullo4@gmail.com>
2021-01-31 12:02:55 -0600

    first commit

---
+++ lib/stock_cli.rb
@@ -0,0 +1,9 @@
+require "stock_cli/version"
+require "pry"
+## require all files here
+## require all "Toolsets/libraries"
+
+module StockCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
