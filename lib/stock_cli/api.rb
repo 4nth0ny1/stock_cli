78f167ae69e387e8789108a1894a15b6ca671352
4anth0ny1 <acatullo4@gmail.com>
2021-02-02 11:21:59 -0600

    finalize errors and refactored cli

--- lib/stock_cli/api.rb
+++ lib/stock_cli/api.rb
@@ -7,20 +7,23 @@ class Api
         @url_2 = 'batch?types=quote,news,chart&range=1m&last=10&token=pk_64a3b816353a4d439a1ab35e94c99d32'
     end 
 
-    def search_company_by_symbol(sym)
-        req_url = "#{url}/#{sym}/#{url_2}"
+    def search_company_by_symbol(input)
+        req_url = "#{url}/#{input}/#{url_2}"
         data = HTTParty.get(req_url)
 
-        stock_hash = {
-            symbol: data["quote"]["symbol"],
-            companyName: data["quote"]["companyName"],
-            close: data["quote"]["close"],
-            volume: data["quote"]["volume"],
-            peRatio: data["quote"]["peRatio"],
-            marketCap: data["quote"]["marketCap"]
-        }   
+        if data["quote"] 
+        
+            stock_hash = {
+                symbol: data["quote"]["symbol"],
+                companyName: data["quote"]["companyName"],
+                close: data["quote"]["close"],
+                volume: data["quote"]["volume"],
+                peRatio: data["quote"]["peRatio"],
+                marketCap: data["quote"]["marketCap"]
+            }   
     
-        stock = Stock.new(stock_hash) 
-        stock
+            stock = Stock.new(stock_hash) 
+            stock
+        end
     end 
 end
--- lib/stock_cli/cli.rb
+++ lib/stock_cli/cli.rb
@@ -1,5 +1,5 @@
 class Cli 
-    
+
     def call
         greeting 
         run 
@@ -9,57 +9,58 @@ class Cli
         puts ""
         puts "Welcome to Stock_cli"
     end 
-    
+
+    def basic_info(s)
+        if s.is_a?(Stock)
+            puts ""
+            puts s.companyName 
+            print "Symbol: "
+            puts s.symbol
+            print "Price: $"
+            puts s.close 
+        else  
+            puts ""  
+            puts "Invalid Entry. Try again"
+            run
+        end
+    end 
+
+    def more_info(second_input, s)
+        if second_input == "yes"
+            puts ""
+            print "Market Cap: "
+            puts s.marketCap
+            print "Volume: "
+            puts s.volume
+            print "P/E Ratio: "
+            puts s.peRatio
+            run
+        elsif second_input == "new stock"
+            run
+        elsif second_input == "exit"
+            puts "Thanks for stopping by."
+            Kernel.exit!
+        elsif second_input != "exit" ||second_input != "new stock" ||second_input != "yes"
+            puts "Invalid Entry. Try again"
+            run
+        end
+    end
+
     def run 
         stock_api = Api.new()
-
         puts ""
         print "Enter a stock symbol: "
-
         input = gets.strip.downcase
-            s = stock_api.search_company_by_symbol(input)
-            if s.is_a?(Stock)
-                Stock.all.each do |s| 
-                    puts ""
-                    puts s.companyName 
-                    print "Symbol: "
-                    puts s.symbol
-                    print "Price: $ "
-                    puts s.close
-                end
-            else    
-                puts "Invalid Entry. Try again"
-                run
-            end
-
+        s = stock_api.search_company_by_symbol(input)
+        basic_info(s)
         puts ""
         puts "Type 'yes' for more information."
         puts "Type 'new stock' to choose another stock."
         puts "Type 'exit' to exit the program."
         puts ""
-
-            second_input = gets.strip.downcase
-                if second_input == "yes"
-                    Stock.all.each do |s|
-                        puts ""
-                        print "Market Cap: "
-                        puts s.marketCap
-                        print "Volume: "
-                        puts s.volume
-                        print "P/E Ratio: "
-                        puts s.peRatio
-                        run
-                    end 
-                elsif second_input == "new stock"
-                    run
-                elsif second_input == "exit"
-                    puts "Thanks for stopping by."
-                    Kernel.exit!
-                elsif second_input != "exit" ||second_input != "new stock" ||second_input != "yes"
-                    puts "Invalid Entry. Try again"
-                    run
-                end
+        second_input = gets.strip.downcase
+        more_info(second_input, s)
     end 
-
-    
 end 
+
+
